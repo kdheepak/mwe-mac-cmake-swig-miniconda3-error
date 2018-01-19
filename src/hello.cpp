d48673be595fad069bc1696cad3595a288491652
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-01-18 20:22:02 -0700

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#if !defined(HELLO_Export)
+#if defined _WIN32 || defined __CYGWIN__
+/* Note: both gcc & MSVC on Windows support this syntax. */
+#define HELLO_Export __declspec(dllexport)
+#else
+#define HELLO_Export __attribute__((visibility("default")))
+#endif
+#endif
+
+HELLO_Export void hello(void);
+
 void hello(void)
 {
 
