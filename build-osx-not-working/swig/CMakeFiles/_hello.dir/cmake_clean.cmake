f229bfec2e1697c382c844bd6dd184255f6742b6
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-01-23 10:33:25 -0700

    Add not-working folder

---
+++ build-osx-not-working/swig/CMakeFiles/_hello.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "helloPYTHON_wrap.c"
+  "hello.py"
+  "helloPYTHON_wrap.c"
+  "hello.py"
+  "CMakeFiles/_hello.dir/helloPYTHON_wrap.c.o"
+  "_hello.pdb"
+  "_hello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
